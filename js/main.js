bd65bbce522e8084c38cb76d86346a237217d163
Nate Lynch <natelynch@Nates-MacBook-Pro.local>
2020-08-01 21:49:53 -0400

    Adding pseudo code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 /*----- constants -----*/
-
+// Define require constants
 
 
 /*----- app's state (variables) -----*/
-
+// Define required variables to track the state of the game
 
 
 /*----- cached element references -----*/
-
+// Store elements on the page that will be accessed in code more than once in variables 
+// to make code more concise, readable and performant.
 
 
 /*----- event listeners -----*/
+// Handle a player clicking a square
 
+// Handle a player clicking the replay button
 
 
 /*----- functions -----*/
-
+// 
 
